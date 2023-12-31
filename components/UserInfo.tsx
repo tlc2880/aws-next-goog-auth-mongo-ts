f63b560b48eaea7a53a07857f8716538f17f948f
tlc2880 <tommylcao@gmail.com>
2023-12-31 05:06:52 -0800

    Add UserInfo.tsx name and email

--- components/UserInfo.tsx
+++ components/UserInfo.tsx
@@ -1,12 +1,30 @@
 "use client";
-
+import { useSession } from "next-auth/react"
 import Image from "next/image";
 import SignInBtn from "./SignInBtn";
 
 export default function UserInfo() {
-  return (
-    <div>
-      <SignInBtn />
+  const { status, data: session } = useSession();
+
+  if (status === 'authenticated') {
+    return (
+    <div className="shadow-xl p-8 rounded-md flex flex-col gap-3 bg-yellow-300">
+      {session?.user?.image && <Image
+          className="rounded-full"
+          src={session?.user?.image}
+          alt="User Info"
+          width={60}
+          height={60}
+      />}
+      <div>
+        Name: <span className="font-bold">{session?.user?.name}</span>
+      </div>
+      <div>
+        Email: <span className="font-bold">{session?.user?.email}</span>
+      </div>
     </div>
-  )
+  ); 
+} else {
+    return <SignInBtn />
+  }
 }
