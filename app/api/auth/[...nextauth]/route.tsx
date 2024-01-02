abca30f732ef45705f9dc311b41819d37a8255f6
tlc2880 <tommylcao@gmail.com>
2024-01-02 08:15:22 -0800

    Add [...nextauth]/route callbacks

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,7 +1,15 @@
-import NextAuth from "next-auth/next"
+import NextAuth from "next-auth/next";
+import User from "@/models/user";
+import { connectMongoDB } from "@/lib/mongodb";
 import GoogleProvider from 'next-auth/providers/google';
-let clientId = process.env.GOOGLE_CLIENT_ID as string;
-let clientSecret =  process.env.GOOGLE_CLIENT_SECRET as string;
+
+const clientId = process.env.GOOGLE_CLIENT_ID as string;
+const clientSecret =  process.env.GOOGLE_CLIENT_SECRET as string;
+
+type Props = {
+  user: any, 
+  account: any
+}
 
 const authOptions = {
   providers: [
@@ -10,6 +18,37 @@ const authOptions = {
       clientSecret,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account }: Props) {
+      if (account.provider === "google") {
+        const { name, email } = user;
+        try {
+          await connectMongoDB();
+          const userExists = await User.findOne({ email });
+
+          if (!userExists) {
+            const res = await fetch("http://localhost:3000/api/user", {
+              method: "POST",
+              headers: {
+                "content-Type": "application/json",
+              },
+                body: JSON.stringify({
+                  name,
+                  email,
+                }),
+              });
+
+              if (res.ok) {
+                return user;
+              }
+            }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      return user;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import UserInfo from '@/components/UserInfo';
 
 export default function Home() {
